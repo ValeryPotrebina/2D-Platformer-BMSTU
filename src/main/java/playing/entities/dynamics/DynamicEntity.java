07bf77d40f38b54874bcc8161e28a66a27619135
ArkadiyShevyrov <108514147+ArkadiyShevyrov@users.noreply.github.com>
2022-08-18 12:18:28 +0300

    Add dynamic and static Entity

---
+++ src/main/java/playing/entities/dynamics/DynamicEntity.java
@@ -0,0 +1,9 @@
+package playing.entities.dynamics;
+
+import playing.entities.Entity;
+
+public abstract class DynamicEntity extends Entity {
+    public DynamicEntity(double x, double y) {
+        super(x, y);
+    }
+}
---
+++ src/main/java/playing/entities/statics/StaticEntity.java
@@ -0,0 +1,9 @@
+package playing.entities.statics;
+
+import playing.entities.Entity;
+
+public abstract class StaticEntity extends Entity {
+    public StaticEntity(double x, double y) {
+        super(x, y);
+    }
+}
